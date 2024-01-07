be0c0ba0315fc40266382dfd3a44a7fe386b287d
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-07 19:18:11 +0200

    counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-export const Counter = () => (
-  <div>
-    <span>0</span>
-    <div>
-      <button type="button">Збільшити на 1</button>
-      <button type="button">Зменшити на 1</button>
-    </div>
-  </div>
-);
+export class Counter extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState(prevState => {
+      return {
+        value: prevState.value + 1,
+      };
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => {
+      return {
+        value: prevState.value - 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.value}</span>
+        <div>
+          <button type="button" onClick={this.handleIncrement}>
+            Збільшити на 1
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            Зменшити на 1
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
