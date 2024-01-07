0024572e8c49c06c7533801f3d1b99c1b771206e
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-07 18:47:13 +0200

    remove

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Counter = () => (
+  <div>
+    <span>0</span>
+    <div>
+      <button type="button">Збільшити на 1</button>
+      <button type="button">Зменшити на 1</button>
+    </div>
+  </div>
+);
